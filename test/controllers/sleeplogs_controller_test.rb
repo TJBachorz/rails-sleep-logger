ca4ac7731c319599a1f8bcbfeef467d8c1a7cbf3
TJ <tjbachorz@gmail.com>
2022-01-23 12:32:04 -0700

    runs simple integrations tests

--- test/controllers/sleeplogs_controller_test.rb
+++ test/controllers/sleeplogs_controller_test.rb
@@ -1,21 +1,14 @@
 require 'test_helper'
 
 class SleeplogsControllerTest < ActionDispatch::IntegrationTest
-  test "that a sleeplog is valid" do
-    log = Sleeplog.new(
-      night_of_date: "10/20/2021",
-      bedtime: "11:55:00 PM",
-      approximate_sleep_time: "12:40:00 AM",
-      sleep_interrupted: 1,
-      sleep_lost_from_interruptions: 0.50,
-      wake_up_time: "08:45:00 AM",
-      ate_spicy: true,
-      drank_alcohol: false,
-      sleep_meds: true,
-      user_id: 6,
-      notes: "slept well",
-      useMarijuana: false
-    )
-    assert log.valid?, "Didn't save valid sleeplog"
+  test "the sleeplogs index" do
+    get sleeplogs_url
+    assert_response :success
+  end
+
+  test "sleeplog show method" do
+    get sleeplogs_url(Sleeplog.first), headers: { "HTTP_REFERRER" => 
+    "http://localhost:3000/sleeplogs/5"}
+    assert_response :success
   end
 end
